517574856c17b8dbd0361a9ee000032059a76e85
root <root@ip-172-31-18-160.ec2.internal>
2026-04-16 06:59:17 +0000

    this is my second commit

--- file.java
+++ file.java
@@ -34,4 +34,24 @@ public class SpiralPatternExample2 {
         printPattern(n);
     }
 }
+  // driver code
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        System.out.println();
+
+        // function calling
+        printPattern(n);
+  // driver code
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        System.out.println();
+
+        // function calling
+        printPattern(n);
 
