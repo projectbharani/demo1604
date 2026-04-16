4c52289f759ba7783971f8d9a8084fe2d8f22de8
root <root@ip-172-31-18-160.ec2.internal>
2026-04-16 07:04:21 +0000

    this is my third commit

--- file.java
+++ file.java
@@ -1,3 +1,16 @@
+    // driver code
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        System.out.println();
+
+        // function calling
+        printPattern(n);
+    }
+}
+
 import java.io.*;
 import java.util.Scanner;
 
